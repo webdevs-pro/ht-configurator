c00d310577a5ea46a2a96d1a7a4c801a38e9c981
WebDevs.pro <shock_31337@ukr.net>
2023-07-25 09:11:23 +0300

    0.7.3

--- class-configurator-shortcode.php
+++ class-configurator-shortcode.php
@@ -192,11 +192,11 @@ class HT_Configurator {
 										echo '</label>';
 									}
 
-									if ( $options_group['section_popup_text'] ) {
+									if ( isset( $options_group['section_popup_text'] ) && $options_group['section_popup_text'] ) {
 										echo '<a href="#" class="dtc-fieldset-popup-open">Weitere Infos</a>';
 										echo '<div class="dtc-fieldset-popup">';
 											echo '<div class="dtc-fieldset-popup-wrapper">';
-												echo '<p class="dtc-fieldset-popup-heading">' . $options_group['section_popup_text'] ?? '' . '</p>';
+												echo '<p class="dtc-fieldset-popup-heading">' . $options_group['section_popup_heading'] ?? '' . '</p>';
 												echo '<p class="dtc-fieldset-popup-text">' . $options_group['section_popup_text'] . '</p>';
 												echo '<button class="dtc-fieldset-popup-ok">Ok</button>';
 											echo '</div>';
